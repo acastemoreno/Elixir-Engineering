a4694701909bec0308d985ff5238b18c3e52b44c
acastemoreno <acastemoreno@gmail.com>
2016-11-03 02:25:12 -0500

    4ta session

---
+++ 4ta session/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Programación funcional con Elixir y Phoenix orientado a Ingeniería', 
+     subtitle: 'OTP, Plug y Phoenix', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/phoenix.svg', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Albert Castellano Moreno', 
+     company: '', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: 'http://github.com/acastemoreno', 
+   }]
+};
